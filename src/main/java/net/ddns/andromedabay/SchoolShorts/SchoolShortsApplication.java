7301a2bbb7932f86a450762db24fd8ac2b218737
hammadrauf <rauf.hammad@gmail.com>
2023-06-22 23:57:37 -0400

    first commit

---
+++ src/main/java/net/ddns/andromedabay/SchoolShorts/SchoolShortsApplication.java
@@ -0,0 +1,13 @@
+package net.ddns.andromedabay.SchoolShorts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolShortsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolShortsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ddns/andromedabay/SchoolShorts/SchoolShortsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ddns.andromedabay.SchoolShorts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolShortsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
