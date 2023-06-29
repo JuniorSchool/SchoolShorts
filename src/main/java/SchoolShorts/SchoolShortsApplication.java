7d25ec35c2db070da672ccb22dc0a2ff819c80e2
hammadrauf <rauf.hammad@gmail.com>
2023-06-29 18:56:13 -0400

    jar file is runnable now

--- src/main/java/SchoolShorts/SchoolShortsApplication.java
+++ src/main/java/SchoolShorts/SchoolShortsApplication.java
@@ -2,6 +2,8 @@ package SchoolShorts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SchoolShortsApplication {
@@ -10,4 +12,12 @@ public class SchoolShortsApplication {
 		SpringApplication.run(SchoolShortsApplication.class, args);
 	}
 
+        @Bean
+        public ServletRegistrationBean countingTablesServletBean() {
+            ServletRegistrationBean bean = new ServletRegistrationBean(
+              new ServletPDFCountingTable(), "/ServletPDFCountingTable");
+            bean.setLoadOnStartup(1);
+            return bean;
+        }        
+        
 }
